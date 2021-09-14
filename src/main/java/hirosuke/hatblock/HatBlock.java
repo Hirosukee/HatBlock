f8c4d926b1d5ef3b2c24d457fa156f1b171db0e1
Hirosuke <senolynn7584@gmail.com>
2021-09-14 20:32:59 +0900

    init

---
+++ src/main/java/hirosuke/hatblock/HatBlock.java
@@ -0,0 +1,17 @@
+package hirosuke.hatblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HatBlock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
