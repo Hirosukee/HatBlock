b3d4baa9d09f075f67874cf8e40565f27bb7d7e2
Hirosuke <51872161+Hirosukee@users.noreply.github.com>
2021-09-14 23:09:58 +0900

    refactoring

--- src/main/java/hirosuke/hatblock/CommandHatBlock.kt
+++ src/main/java/hirosuke/hatblock/CommandHatBlock.kt
@@ -9,12 +9,10 @@ class CommandHatBlock : CommandExecutor {
 
     @Override
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        if (command.isRegistered) {
-            if(sender is Player) {
-                sender.inventory.helmet = sender.inventory.itemInMainHand
-                sender.sendMessage("§f§l§oHat!")
-                return true
-            }
+        if (command.isRegistered and (sender is Player)) {
+            sender.inventory.helmet = sender.inventory.itemInMainHand
+            sender.sendMessage("§f§l§oHat!")
+            return true
         }
         return false
     }
