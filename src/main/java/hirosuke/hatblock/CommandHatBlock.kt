3a4ef88ecbf56848e5aaed3fe385a21748bd74b3
Hirosuke <senolynn7584@gmail.com>
2021-09-14 21:42:03 +0900

    dekita

---
+++ src/main/java/hirosuke/hatblock/CommandHatBlock.kt
@@ -0,0 +1,22 @@
+package hirosuke.hatblock
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class CommandHatBlock : CommandExecutor {
+
+    @Override
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (command.isRegistered) {
+            if(sender is Player) {
+                sender.inventory.helmet = sender.inventory.itemInMainHand
+                sender.sendMessage("§f§l§oHat!")
+                return true
+            }
+        }
+        return false
+    }
+
+}
--- src/main/java/hirosuke/hatblock/HatBlock.kt
+++ src/main/java/hirosuke/hatblock/HatBlock.kt
@@ -2,12 +2,15 @@ package hirosuke.hatblock;
 
 import org.bukkit.plugin.java.JavaPlugin
 
-class KotlinPlugin : JavaPlugin() {
+class HatBlock : JavaPlugin() {
 
+        @Override
         override fun onEnable() {
+                getCommand("hat").setExecutor(CommandHatBlock())
                 logger.info("plugin has loaded.")
         }
 
+        @Override
         override fun onDisable() {
                 logger.info("plugin has unloaded.")
         }
